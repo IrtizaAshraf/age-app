5a8911f9e13ab35c13e6234c4da4460d3eaa7736
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-09-08 01:41:34 +0500

    done

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var currentAge = prompt("Enter your age.");
+var qualifyingAge = parseInt(currentAge) + 1;
+
+// console.log(currentAge);
+console.log();
+console.log(~19);
+
+
+console.log(currentAge*1);
